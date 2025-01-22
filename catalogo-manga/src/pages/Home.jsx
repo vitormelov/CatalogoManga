e6c7a867f1caed35e53d8b6c598d868e1ba24329
Vitor Melov <vitormelov@gmail.com>
2025-01-21 22:02:57 -0300

    first push

---
+++ catalogo-manga/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../style/Home.css';
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    navigate('/login'); // Redireciona para a página de login
+  };
+
+  return (
+    <div className="home">
+      <video className="background-video" autoPlay loop muted>
+        <source src="/videos/background.mp4" type="video/mp4" />
+        Seu navegador não suporta vídeos HTML5.
+      </video>
+      <div className="home-content">
+        <h1>Catálogo de Mangás</h1>
+        <p>Bem-vindo ao seu organizador digital de mangás! Aqui você pode catalogar, explorar e gerenciar suas coleções favoritas.</p>
+        <button onClick={handleStart}>COMEÇAR</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ catalogo-manga/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Insira suas credenciais para acessar o Catálogo de Mangás.</p>
+    </div>
+  );
+};
+
+export default Login;
