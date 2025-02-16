e2f6dc3d3275bb2c3b9102fe150c7a9e878121e1
Vitor Melov <vitormelov@gmail.com>
2025-02-16 17:25:09 -0300

    cors

--- catalogo-manga-backend/server.js
+++ catalogo-manga-backend/server.js
@@ -1,11 +1,20 @@
-const app = require('./src/app');
+const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const app = require('./src/app');
 
 // Carregar variáveis de ambiente
 dotenv.config();
 
+// Configuração do CORS para permitir o frontend no Vercel
+app.use(cors({
+  origin: 'https://catalogo-manga.vercel.app', // Permite apenas o frontend no Vercel
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`✅ Servidor rodando na porta ${PORT}`);
 });
