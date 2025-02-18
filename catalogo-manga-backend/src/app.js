4223c7883613e8732e34758b5ae5e1b83fc2dc58
Vitor Melov <vitormelov@gmail.com>
2025-02-17 22:16:06 -0300

    patch 17.02.25.2215

--- catalogo-manga-backend/src/app.js
+++ catalogo-manga-backend/src/app.js
@@ -15,7 +15,7 @@ const allowedOrigins = ['https://catalogo-manga.vercel.app'];
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Origin', 'https://catalogo-manga.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
